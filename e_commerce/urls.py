a090b0bf8870a04b6a96ab695a57834aa2fd4b28
Argao <jgabrielaragao@gmail.com>
2024-12-08 13:42:40 -0300

    Imagens

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('new_compra/', views.new_compra, name='new_compra'),
     path('new_cliente/', views.new_cliente, name='new_cliente'),
     path('pulseiras/', views.pulseiras, name='pulseiras'),
-    path('pulseira/<pulseira_id>', views.show_pulseira, name='pulseira'),
-    path('delete_pulseira/<pulseira_id>', views.delete_pulseira, name='delete_pulseira'),
-    path('update_pulseira/<pulseira_id>', views.update_pulseira, name='update_pulseira'),
+    path('pulseira/<pulseira_id>/', views.show_pulseira, name='pulseira'),
+    path('delete_pulseira/<pulseira_id>/', views.delete_pulseira, name='delete_pulseira'),
+    path('update_pulseira/<pulseira_id>/', views.update_pulseira, name='update_pulseira'),
 ]
